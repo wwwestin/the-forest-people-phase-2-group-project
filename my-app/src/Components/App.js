65b0056a2d881b97dea55e4264005adc399eb598
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-07 17:05:25 -0600

    working base

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
->>>>>>> 0b55de284d8455049110b1059fdfb67ac62c6118
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import NationalParksContainer from "./NationalParksContainer";
@@ -15,30 +12,15 @@ import WhereIveBeen from "./WhereIveBeen";
 function App() {
 
     return (
-<<<<<<< HEAD
-        <div>
-            <div>
-=======
         <Router>
->>>>>>> 0b55de284d8455049110b1059fdfb67ac62c6118
             <Header />
-            </div>
-            <nav>
             <Navbar />
-<<<<<<< HEAD
-            </nav>
-            <container>
-                <NationalParksContainer />
-            </container>
-        </div>
-=======
             <Routes>
                 <Route exact path="/" element={<NationalParksContainer />} />
-                <Route path="/where-im-going" component={<WhereImGoing />} />
-                <Route path="/where-ive-been" component={<WhereIveBeen />} />
+                <Route path="/where-im-going" element={<WhereImGoing />} />
+                <Route path="/where-ive-been" element={<WhereIveBeen />} />
             </Routes>
         </Router>
->>>>>>> 0b55de284d8455049110b1059fdfb67ac62c6118
     )
 
 

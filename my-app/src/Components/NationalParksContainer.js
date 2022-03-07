f521b437d924e6efd0a9a12dccd8746a5ba20927
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-03-07 15:52:22 -0600

    load more button

--- my-app/src/Components/NationalParksContainer.js
+++ my-app/src/Components/NationalParksContainer.js
@@ -1,40 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import NationalPark from "./NationalPark";
 
-const BASE_URL = 'https://developer.nps.gov/api/v1/parks?api_key=3nGt9ZQTH0fW8byyMhNt9bA1avBgXX7gbGuT7Rt4&limit=9'
+const BASE_URL = 'https://developer.nps.gov/api/v1/parks?api_key=3nGt9ZQTH0fW8byyMhNt9bA1avBgXX7gbGuT7Rt4&limit='
 
 
 function NationalParksContainer() {
-    
+
     const [parks, setParks] = useState([])
+    const [limitNum, setLimitNum] = useState(9)
 
     useEffect(() => {
-        fetch(BASE_URL)
-        .then(res => {
-            console.log(res)
-            return res.json()
-        })
-        .then(data => {
-            setParks(data.data)
-        })
-    }, [])
+        fetch(BASE_URL + limitNum)
+            .then(res => {
+
+                return res.json()
+            })
+            .then(data => {
+                setParks(data.data)
+            })
+    }, [limitNum])
 
     const nationalParkComponents = parks.map(park => {
-        console.log(park)
+
         return (
             <NationalPark
                 key={park.id}
                 name={park.fullName}
-                imageUrl = {park.images[0].url}
-                imageAlt = {park.images[0].altText}
+                imageUrl={park.images[0].url}
+                imageAlt={park.images[0].altText}
             />
         )
     })
 
-    console.log(nationalParkComponents)
 
     return <div className="cards-container">
         {nationalParkComponents}
+        <button onClick={() => setLimitNum(limitNum => limitNum + 9)}>LoadMore</button>
     </div>
 }
 

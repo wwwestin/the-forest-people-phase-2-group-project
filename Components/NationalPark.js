d38c628a99f0e1130e2456ed0cf65f19a9ecf161
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-07 10:25:23 -0600

    added components

---
+++ Components/NationalPark.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function NationalPark() {
+
+    return "national park"
+}
+
+export default  NationalPark;
---
+++ Components/NationalParksContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NationalPark from "./NationalPark";
+
+function NationalParksContainer() {
+    
+    return (
+        <NationalPark />
+    )    
+}
+
+export default NationalParksContainer;

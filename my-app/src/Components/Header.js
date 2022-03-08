6636f2ebce58f032d31665e3996f13928da6588a
wwwestin <westin.h@gmail.com>
2022-03-07 21:20:21 -0500

    moved modified dropdown/search to NavBar

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Caravan from "../caravan-solid.svg"
-import DropDown from "./DropDown";
+
 function Header() {
 
     return (
         <div className = "header">
             <h1>Road Trippin'</h1>
             <img src={Caravan}/>
-            <DropDown />
         </div>
     )
 }
